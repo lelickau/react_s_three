76cea6f7e849cd74605b5d390c8205e879344d01
elena_kau <lelickau@yandex.ru>
2021-04-29 14:51:26 +0200

    v1.0

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+    return(
+        <div className="loadingio-spinner-eclipse-02egaw7pgr8r">
+            <div className="ldio-8psl2bq2zii">
+                <div></div>
+            </div>
+        </div>
+    )
+}
+
+export default Spinner;
